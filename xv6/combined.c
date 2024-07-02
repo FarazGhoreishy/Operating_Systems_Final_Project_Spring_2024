45d69886ad3022e3af9c67df868a6be66f298bb4
FarazGhoreishy <farazghoreishy@gmail.com>
2024-07-02 22:54:59 +0330

    Test code that combines image and mat_mul

---
+++ xv6/combined.c
@@ -0,0 +1,36 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main() 
+{
+    for (int i = 0; i < 4; i++) 
+    {
+        int pid = fork();
+
+        if (pid  == 0) 
+        { 
+            if (i % 2)
+            {
+                char *args[] = {"image", 0};
+                exec("image", args);
+            }
+            else
+            { 
+                char *args[] = {"mat_mul", 0};
+                exec("mat_mul", args);
+            }
+            exit(); 
+        }
+        if (pid > 0)
+        {   
+            int wpid;
+            while ((wpid = wait()) > 0);
+        }
+    }
+    
+    csc(getpid());
+    // ctat(getpid());
+    printf(1, "Turnaround Time for this process is %d\n", ctat());
+    exit();
+}
